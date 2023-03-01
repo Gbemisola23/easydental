8ac3e94ba1405e79991ae375ca25403f804c86d5
Gbemisola Ajebeku <gbemisolaje@gmail.com>
2023-03-01 17:31:00 +0000

    created the home page view

--- booking/views.py
+++ booking/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.generic.base import TemplateView
 
 
-class HomeTemplateView(TemplateView):
-    template_name = "index.html"
-
+def home(request):
+    return render(request, 'home.html', {})
